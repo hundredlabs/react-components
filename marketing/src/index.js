3985b6edd1a8fb73c854c8f2310a6fd0c1d508c3
sarajul <sarajulmd5@gmail.com>
2023-03-10 17:59:01 +0530

    Add minor changes in alloyNav

--- marketing/src/index.js
+++ marketing/src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import AlloyNav from "./sections/navigation/AlloyNav/AlloyNav";
 import reportWebVitals from "./reportWebVitals";
 import HeadStream from "./sections/hero/HeadStream/HeadStream";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HeadStream />
+    <AlloyNav />
   </React.StrictMode>
 );
 
---
+++ marketing/src/sections/navigation/AlloyNav/AlloyNav.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./AlloyNav.css"
+
+const AlloyNav = () => {
+
+    return (
+
+        <nav>
+            <h1>alloy</h1>
+            <ul>
+                <a>Products</a> <select></select>
+            </ul>
+
+            <ul>
+                <a>Integrations</a>
+            </ul>
+            <ul>
+                <a>Recipes</a>
+            </ul>
+
+            <ul>
+                <a>Resources</a> <select></select>
+            </ul>
+        </nav>
+
+    )
+
+
+}
+
+export default AlloyNav;
