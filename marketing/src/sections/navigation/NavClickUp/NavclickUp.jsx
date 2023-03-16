c7f9c611a87021d0fe1c1800b24302a0309d252b
Zeeshan@479 <zakhter449@gmail.com>
2023-03-16 22:57:49 +0530

    added some css rules in clickup navigation

--- marketing/src/sections/navigation/NavClickUp/NavclickUp.jsx
+++ marketing/src/sections/navigation/NavClickUp/NavclickUp.jsx
@@ -3,72 +3,134 @@ import React from 'react';
 
 const ProductBar = () => (
     <div className="menuList">
-        <div className='menuBar'>
-            <div className='list'>
-                <div>
-                    <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
+        <div>
+            <h1>PRODUCT</h1>
+            <span className='newTag'>new</span>
+            <div className='header'>
+                <div className='header-logo'>
+                    <img className='img-header' src='./NavBarImg/click.png' alt="logo" />
+                    <p4>ClichUp</p4><p2>3.0</p2>
                 </div>
-                <div>
-                    <h1>Tasks</h1>
-                    <p>Break down any size product</p>
-                    <p>Break down any size product</p>
-                </div>
-            </div>
-            <div className='list'>
-                <div>
-                    <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
-                </div>
-                <div>
-                    <h1>Docs</h1>
-                    <p>Break down any size product</p>
-                    <p>Break down any size product</p>
+                <div className='header-sentence'>
+                    Unlock the next generation of productivity<p3> →</p3>
                 </div>
             </div>
-            <div className='list'>
-                <div>
-                    <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
-                </div>
-                <div>
-                    <h1>Goals</h1>
-                    <p>Break down any size product</p>
-                    <p>Break down any size product</p>
-                </div>
-            </div>
-        </div>
+            <div className='menuBar'>
+                <div className='list'>
+                    <div>
+                        <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
+                    </div>
+                    <div>
+                        <h1>Tasks</h1>
 
-        <div className='menuBar'>
-            <div className='list1'>
-                <div>
-                    <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
+                        <p>Break down any size product</p>
+                    </div>
                 </div>
-                <div>
-                    <h1>WhiteBoard</h1>
-                    <p>Break down any size product</p>
-                    <p>Break down any size product</p>
+                <div className='list'>
+                    <div>
+                        <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
+                    </div>
+                    <div>
+                        <h1>Docs</h1>
+
+                        <p>Break down any size product</p>
+                    </div>
+                </div>
+                <div className='list'>
+                    <div>
+                        <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
+                    </div>
+                    <div>
+                        <h1>Goals</h1>
+                        <p>Break down any size product</p>
+                    </div>
                 </div>
             </div>
-            <div className='list1'>
+
+            <div className='menuBar'>
+                <div className='list1'>
+                    <div>
+                        <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
+                    </div>
+                    <div>
+                        <h1>WhiteBoard</h1>
+
+                        <p>Break down any size product</p>
+                    </div>
+                </div>
+                <div className='list1'>
+                    <div>
+                        <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
+                    </div>
+                    <div>
+                        <h1>Dashboard</h1>
+                        <p6>Break down any size product</p6>
+                    </div>
+                </div>
+                <div className='list1'>
+                    <div>
+                        <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
+                    </div>
+                    <div>
+                        <h1>Chat</h1>
+                        <p>Break down any size product</p>
+                    </div>
+                </div>
+            </div>
+            <button>See All Features    <div className='btnP'>→</div></button>
+        </div>
+        <div>
+            <h1>Platform</h1>
+            <div className='platform'>
                 <div>
                     <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
                 </div>
                 <div>
                     <h1>Dashboard</h1>
-                    <p>Break down any size product</p>
-                    <p>Break down any size product</p>
+                    <p6>Break down any size product</p6>
                 </div>
-            </div>
-            <div className='list1'>
                 <div>
                     <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
                 </div>
                 <div>
-                    <h1>Chat</h1>
-                    <p>Break down any size product</p>
-                    <p>Break down any size product</p>
+                    <h1>Dashboard</h1>
+                    <p6>Break down any size product</p6>
+                </div>
+            </div>
+            <div className='platform'>
+                <div>
+                    <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
+                </div>
+                <div>
+                    <h1>Dashboard</h1>
+                    <p6>Break down any size product</p6>
+                </div>
+                <div>
+                    <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
+                </div>
+                <div>
+                    <h1>Dashboard</h1>
+                    <p6>Break down any size product</p6>
+                </div>
+            </div>
+            <div className='platform'>
+                <div>
+                    <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
+                </div>
+                <div>
+                    <h1>Dashboard</h1>
+                    <p6>Break down any size product</p6>
+                </div>
+                <div>
+                    <img className='img-list' src='./NavBarImg/logo192.png' alt="Tash-logo" />
+                </div>
+                <div>
+                    <h1>Dashboard</h1>
+                    <p6>Break down any size product</p6>
                 </div>
             </div>
         </div>
-    </div >
+    </div>
 )
 
 
