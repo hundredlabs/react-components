9072cb7b5b069cb95fb0635b22d941ce9f587abc
sarajul <sarajulmd5@gmail.com>
2023-03-20 11:02:09 +0530

    cleaned repo

--- marketing/src/index.js
+++ marketing/src/index.js
@@ -3,18 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import AlloyNav from "./sections/navigation/AlloyNav/AlloyNav";
-import HeadStream from "./sections/hero/HeadStream/HeadStream";
-import BlueSimple from "./sections/navigation/Blue-Simple/Blue-Simple";
+import OmnisendNav from "./sections/navigation/OmnisendNav/OmnisendNav";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
+    <OmnisendNav />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
