4a6af261f8affe3ef96d372f1cc7efe53609bb84
Zeeshan@479 <zakhter449@gmail.com>
2023-03-22 14:09:24 +0530

    changed the classname of nav tag.

--- marketing/src/index.js
+++ marketing/src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import AlloyNav from "./sections/navigation/AlloyNav/AlloyNav";
-import HeadStream from "./sections/hero/HeadStream/HeadStream";
-import BlueSimple from "./sections/navigation/Blue-Simple/Blue-Simple";
+import NavclickUp from "./sections/navigation/ClickupNav/ClickupNav";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
+    <NavclickUp />
   </React.StrictMode>
 );
 
