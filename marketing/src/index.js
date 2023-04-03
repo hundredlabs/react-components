9abc10243ef5b2acf9df93a4359aec1b5cd2b739
Zeeshan@479 <zakhter449@gmail.com>
2023-04-03 17:35:33 +0530

    undo index.js

--- marketing/src/index.js
+++ marketing/src/index.js
@@ -4,8 +4,10 @@ import reportWebVitals from "./reportWebVitals";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode>
-</React.StrictMode>);
+root.render(
+    <React.StrictMode>
+    </React.StrictMode>
+);
 
 
 // If you want to start measuring performance in your app, pass a function
